8820821aec017336559099593684b0e008013735
zaidkilani <zaid.a.kilani@gmail.com>
2021-12-05 13:33:45 +0200

    init

---
+++ __manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Codesk Solutions L.L.C.
+#
+#    Copyright (C) 2021-TODAY Codesk solutions(<https://www.Codesksolutions.co>)
+#    Author: Codesk Solutions(<https://www.Codesksolutions.co>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+{  
+    'name': 'SOS Case Managment System',
+    'version': '15.0.0.0.0',
+    'category': 'Extra Tools',
+    'summary': """ case managment for youth and childern """,
+    'description': """
+                    a case managment system where you can add manage report,
+                    childerns and youths that under sos villages
+                    """,
+    'author': 'Codesk Solutions',
+    'website': "https://www.codesksolutions.co",
+    'company': 'Codesk Solutions',
+    'maintainer': 'Codesk Solutions',
+    'depends': [''],
+    'data': [,],
+    'license': 'LGPL-3',
+    'images': ['static/description/banner.gif'],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
